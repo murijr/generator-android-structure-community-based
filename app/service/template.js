25f8dcadeaba34fb684ace7635e0302c15ae3907
ardsj <ardsj@yahoo.com.br>
2017-09-19 23:44:34 -0300

    Corrigindo nomenclatura e estrutura dos modulos

--- app/service/template.js
+++ app/service/template.js
@@ -1,28 +1,32 @@
 const unirest = require('unirest')
 const remoteRepo = require('../repository/template.js')
 
-module.exports
-.getPublicTemplates = (actionSucess, actionError) => {
+module.exports = {
 
-    remoteRepo.getTemplates().then((templates) => {
-
-        try {
-            actionSucess({
-                templatesSimpleList:   formattTemplateCollection(templates),
-                templatesFullInfo:  templates
-            })
-        } catch (error) {
-            actionError(error)            
-        }
-
-    })
-
+    getTemplates: (actionSucess, actionError) => {
+        
+        remoteRepo.getTemplates().then((templates) => {
+    
+            try {
+                actionSucess({
+                    templatesSimpleList:   module.exports.formattTemplateCollection(templates),
+                    templatesFullInfo:  templates
+                })
+            } catch (error) {
+                actionError(error)            
+            }
+    
+        })
+        
+    },
+    
+    formattTemplateCollection: (templateArray) => {
+        var formatedArray = []
+        templateArray.forEach(function(element) {        
+            formatedArray.push(element.title)
+        });
+        return formatedArray
+    }
+        
 }
 
-let formattTemplateCollection = (templateArray) => {
-    var formatedArray = []
-    templateArray.forEach(function(element) {        
-        formatedArray.push(element.title)
-    });
-    return formatedArray
-}
